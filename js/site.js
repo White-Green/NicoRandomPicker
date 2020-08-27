407088a657ec0446cc6a2a5d6e7bafaea0d8b71a
White-Green <43771790+White-Green@users.noreply.github.com>
2020-08-27 19:51:44 +0900

    fix layout

--- js/site.js
+++ js/site.js
@@ -1,7 +1,8 @@
 async function button_pushed() {
     // console.log("pushed!");
+    document.getElementById("submit_button").disabled = true;
+    document.getElementById("hit_count").innerText = "検索中";
     document.getElementById("result").innerHTML = "";
-    document.getElementById("hit_count").innerText = "";
     let tag_name = document.getElementById("tag_name_input").value;
     let video_count = document.getElementById("video_count_input").value;
     fetch("https://nicorandompickerfunction.azurewebsites.net/api/GetVideosId?tag_name=" + encodeURI(tag_name) + "&video_count=" + encodeURI(video_count))
@@ -14,6 +15,14 @@ async function button_pushed() {
             });
             document.getElementById("result").innerHTML = html;
             document.getElementById("hit_count").innerText = "hit " + json.length;
+        })
+        .catch(error => {
+            console.log(error);
+            document.getElementById("hit_count").innerText = "ERROR - " + error;
+        })
+        .finally(() => {
+            // console.log("finally");
+            document.getElementById("submit_button").disabled = false;
         });
 }
 
