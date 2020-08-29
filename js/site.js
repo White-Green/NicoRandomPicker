c009faa5d00a4714287b2046a4fa29f136c15161
White-Green <43771790+White-Green@users.noreply.github.com>
2020-08-29 21:52:08 +0900

    compress params

--- js/site.js
+++ js/site.js
@@ -58,18 +58,101 @@ async function button_pushed() {
     }
 }
 
+function decimal_to_62(value) {
+    if (value === 0) return "0";
+    let result = "";
+    while (value > 0) {
+        let digit = value % 62;
+        value = parseInt(value / 62);
+        if (0 <= digit && digit <= 9) result = String.fromCharCode("0".charCodeAt() + digit) + result;
+        if (10 <= digit && digit <= 35) result = String.fromCharCode("a".charCodeAt() + digit - 10) + result;
+        if (36 <= digit && digit <= 61) result = String.fromCharCode("A".charCodeAt() + digit - 36) + result;
+    }
+    return result;
+}
+
+function decimal_from_62(value) {
+    if (value === "") return null;
+    let result = 0;
+    for (let i = 0; i < value.length; i++) {
+        result *= 62;
+        let c = value.charCodeAt(i);
+        if ("0".charCodeAt() <= c && c <= "9".charCodeAt()) result += c - "0".charCodeAt();
+        if ("a".charCodeAt() <= c && c <= "z".charCodeAt()) result += 10 + c - "a".charCodeAt();
+        if ("A".charCodeAt() <= c && c <= "Z".charCodeAt()) result += 36 + c - "A".charCodeAt();
+    }
+    return result;
+}
+
+function str_to_base64(str) {
+    let utf8str = String.fromCharCode.apply(null, new TextEncoder().encode(str));
+    return btoa(utf8str);
+}
+
+function str_from_base64(data) {
+    const decoded_utf8str = atob(data);
+    const decoded_array = new Uint8Array(Array.prototype.map.call(decoded_utf8str, c => c.charCodeAt()));
+    return new TextDecoder().decode(decoded_array);
+}
+
+function compress_videos(videos) {
+    let result = [];
+    for (const video of videos) {
+        let len = 0;
+        while (video.charCodeAt(len) < "0".charCodeAt() || "9".charCodeAt() < video.charCodeAt(len)) len++;
+        result.push(video.substring(0, len) + (len === 2 ? "" : "_") + decimal_to_62(video.substring(len)));
+    }
+    return result.join("-");
+}
+
+function decompress_videos(str) {
+    let video_list = str.split("-");
+    let result = [];
+    for (const video of video_list) {
+        if (video.includes("_")) {
+            let sp = video.split("_");
+            result.push(sp[0] + decimal_from_62(sp[1]));
+        } else {
+            result.push(video.substring(0, 2) + decimal_from_62(video.substring(2)));
+        }
+    }
+    return result;
+}
+
+function compress_datetime(datetime) {
+    const regex = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}/.compile();
+    if (!regex.test(datetime)) return "";
+    let i = datetime.substring(0, 4)
+        + datetime.substring(5, 7)
+        + datetime.substring(8, 10)
+        + datetime.substring(11, 13)
+        + datetime.substring(14);
+    if (isNaN(Number(i))) return "";
+    return decimal_to_62(i);
+}
+
+function decompress_datetime(data) {
+    let i = decimal_from_62(data).toString();
+    if (i.length > 12) return "";
+    while (i.length < 12) i = "0" + i;
+    return i.substring(0, 4) + "-"
+        + i.substring(4, 6) + "-"
+        + i.substring(6, 8) + "T"
+        + i.substring(8, 10) + ":"
+        + i.substring(10);
+}
+
 function createURL() {
     let data = {
-        version: 1,
-        body: videos,
-        params: {
-            tag_name: document.getElementById("tag_name_input").value,
-            video_count: document.getElementById("video_count_input").value,
-            view_counter_min: document.getElementById("view_counter_min_input").value,
-            view_counter_max: document.getElementById("view_counter_max_input").value,
-            start_time_from: document.getElementById("start_time_from_input").value,
-            start_time_to: document.getElementById("start_time_to_input").value
-        }
+        version: 2,
+        body: compress_videos(videos),
+        params:
+            str_to_base64(document.getElementById("tag_name_input").value)
+            + "_" + decimal_to_62(document.getElementById("video_count_input").value)
+            + "_" + decimal_to_62(document.getElementById("view_counter_min_input").value)
+            + "_" + decimal_to_62(document.getElementById("view_counter_max_input").value)
+            + "_" + compress_datetime(document.getElementById("start_time_from_input").value)
+            + "_" + compress_datetime(document.getElementById("start_time_to_input").value)
     };
     // console.log(data);
     return location.origin + location.pathname + "?data=" + encodeURIComponent(JSON.stringify(data));
@@ -93,6 +176,7 @@ function jsonTryParse(val) {
         return null;
     }
 }
+
 let promise = Promise.resolve(true);
 function make_tweet_button() {
     promise = promise.then(() => {
@@ -139,33 +223,51 @@ window.addEventListener("load", () => {
         // console.log("data is undefined"); 
         return;
     }
-    if (data.version !== undefined || data.version == 1) {
-        if (data.body === undefined) {
-            // console.log("data.body is undefined");
-            return;
-        }
-        let body = data.body;
-        if (!Array.isArray(body)) {
-            // console.log("data.body is not Array");
-            return;
-        }
-        let correct = true;
-        let regex = /\D{2}\d+/.compile();
-        for (const item in body) {
-            correct &= regex.test(item);
-        }
-        if (correct) setResult(body);
-        // else console.log("body is not correct");
+    if (data.version !== undefined) {
+        if (data.version == 1) {
+            if (data.body === undefined) {
+                // console.log("data.body is undefined");
+                return;
+            }
+            let body = data.body;
+            if (!Array.isArray(body)) {
+                // console.log("data.body is not Array");
+                return;
+            }
+            let correct = true;
+            let regex = /\D{2}\d+/.compile();
+            for (const item in body) {
+                correct &= regex.test(item);
+            }
+            if (correct) setResult(body);
+            // else console.log("body is not correct");
 
-        if (data.params !== undefined) {
-            let params = data.params;
-            document.getElementById("tag_name_input").value = params.tag_name;
-            document.getElementById("video_count_input").value = params.video_count;
-            document.getElementById("view_counter_min_input").value = params.view_counter_min;
-            document.getElementById("view_counter_max_input").value = params.view_counter_max;
-            document.getElementById("start_time_from_input").value = params.start_time_from;
-            document.getElementById("start_time_to_input").value = params.start_time_to;
-            make_tweet_button();
+            if (data.params !== undefined) {
+                let params = data.params;
+                document.getElementById("tag_name_input").value = params.tag_name;
+                document.getElementById("video_count_input").value = params.video_count;
+                document.getElementById("view_counter_min_input").value = params.view_counter_min;
+                document.getElementById("view_counter_max_input").value = params.view_counter_max;
+                document.getElementById("start_time_from_input").value = params.start_time_from;
+                document.getElementById("start_time_to_input").value = params.start_time_to;
+                make_tweet_button();
+            }
+        } else if (data.version == 2) {
+            if (data.body !== undefined) {
+                let body = data.body;
+                setResult(decompress_videos(body));
+            }
+            if (data.params !== undefined) {
+                let params = data.params;
+                let p = params.split("_");
+                document.getElementById("tag_name_input").value = str_from_base64(p[0]);
+                document.getElementById("video_count_input").value = decimal_from_62(p[1]);
+                document.getElementById("view_counter_min_input").value = decimal_from_62(p[2]);
+                document.getElementById("view_counter_max_input").value = decimal_from_62(p[3]);
+                document.getElementById("start_time_from_input").value = decompress_datetime(p[4]);
+                document.getElementById("start_time_to_input").value = decompress_datetime(p[5]);
+                make_tweet_button();
+            }
         }
     }
 });
